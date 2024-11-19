05a8424c7bc34868304393cccb2d07047d6b112a
Sumtsov-s_portfolio <kirillsumcov14@gmail.com>
2024-11-19 12:53:35 +0200

    added a function for the mobile munu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,16 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuLinks = document.querySelectorAll(".mobile-menu__menu .link");
+const closeButton = document.querySelector(".mobile-menu__btn-close");
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const closeMenu = () => mobileMenu.classList.remove("is-open");
+
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", closeMenu);
+});
 
 menuBtnOpen.addEventListener('click', toggleMenu);
 menuBtnClose.addEventListener('click', toggleMenu);
